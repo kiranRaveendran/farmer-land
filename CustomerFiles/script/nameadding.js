2d2a497a9b67925bcbe57615ca875a5664b8fd85
krish112345 <krishnanunnigireesan246@gmail.com>
2025-10-31 14:57:26 +0530

    added logout customer

--- CustomerFiles/script/nameadding.js
+++ CustomerFiles/script/nameadding.js
@@ -1,10 +1,37 @@
 let getuser=JSON.parse(localStorage.getItem("currentUser"))||[];
-let currentuser=getuser[0]
+let currentuserr=getuser[0]
+
+
+
 let delt=setInterval(()=>{
     let name=document.getElementById("namecustomer")
+    let btnn=document.getElementById("logout")
+
     if(name){
-        name.textContent=currentuser.name
+        
+        name.textContent=currentuserr.name
+    console.log(name);
+    console.log(currentuserr);
         clearInterval(delt)
-        return
     }
+    if(btnn){
+        btnn.addEventListener("click",(e)=>{
+            e.preventDefault()
+            localStorage.removeItem("currentuser")
+            window.location.href="../loginForm.html"
+        })
+        
+    }
+    return
+
+        // btnn.addEventListener("click",(e)=>{
+        //     e.preventDefault()
+        //     window.location.href="coupon.html"
+        //     console.log(",jxhcg");
+            
+        //     btnn.textContent="ksdkjhc"
+        //     return
+        // })
+        // console.log(btnn.textContent);
+    
 },5)
