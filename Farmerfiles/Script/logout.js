f5450a4848cc57b246f54160bec6ec4a4b19233f
krish112345 <krishnanunnigireesan246@gmail.com>
2025-10-31 16:06:02 +0530

    added farmer logout ,in

--- Farmerfiles/Script/logout.js
+++ Farmerfiles/Script/logout.js
@@ -4,16 +4,7 @@ let currentuserr=getuser[0]
 
 
 let delt=setInterval(()=>{
-    let name=document.getElementById("namecustomer")
     let btnn=document.getElementById("logout")
-
-    if(name){
-        
-        name.textContent=currentuserr.name
-    console.log(name);
-    console.log(currentuserr);
-        clearInterval(delt)
-    }
     if(btnn){
         btnn.addEventListener("click",(e)=>{
             e.preventDefault()
@@ -24,3 +15,4 @@ let delt=setInterval(()=>{
     }
     return 
 },5)
+console.log(getuser);
